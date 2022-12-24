d20b36b7c79d02181c748d4905a6e01137bfff54
Italo Oliveira <italooliveira-xd@hotmail.com>
2022-12-23 21:21:55 -0300

    fixing recipe app paths

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+
+from . import views
 
 urlpatterns = [
-    path('home/', home.views, name=home)
+    path('home/', views.home, name='home')
 ]
