835aa43b6a628fbeb6fc789a9bf5816b8ba393fc
Italo Oliveira <italooliveira-xd@hotmail.com>
2022-12-25 16:45:29 -0300

    configuring and creating the new page for viewing the recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('recipe/<int:id>/', views.recipe, name='recipe'),
 ]
