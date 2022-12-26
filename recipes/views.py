ed1f39826e342f06abcfbc285b01d06dcb69873a
Italo Oliveira <italooliveira-xd@hotmail.com>
2022-12-25 21:06:38 -0300

    some tweaks to the views

--- recipes/views.py
+++ recipes/views.py
@@ -5,12 +5,12 @@ from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', {
-            'recipes': [make_recipe() for _ in range(10)], 
-         })
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', { 
-            'recipe': make_recipe(),
-            'is_detail_page': True,
-        })
+    return render(request, 'recipes/pages/recipe-view.html', {
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
