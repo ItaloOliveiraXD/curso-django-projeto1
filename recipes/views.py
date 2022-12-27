eaf3696ac3b5ac5a663e372efb6d7308e13b4933
Italo Oliveira <italooliveira-xd@hotmail.com>
2022-12-27 10:24:22 -0300

    sending template data to template

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
-from utils.recipes.factory import make_recipe
+# from utils.recipes.factory import make_recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by(
+        '-id'
+    )
+
     return render(request, 'recipes/pages/home.html', {
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+    )
+
+    return render(request, 'recipes/pages/category.html', {
+        'recipes': recipes,
     })
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+
     return render(request, 'recipes/pages/recipe-view.html', {
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
